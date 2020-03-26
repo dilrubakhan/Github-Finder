61649c7f619830397c4c8702f753dfb2e5fe4b0b
Dilruba Khan <rubatanisa@yahoo.ie>
2020-03-26 23:46:11 +0000

    Create list and passing state with props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         <Navbar/>
-        <h1>Hello From React</h1>
+        <div className= "container">
+          <Users/>
+        </div>
+       
       </div>
     );
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -23,4 +23,4 @@ export class Navbar extends Component {
     }
 }
 
-export default Navbar
+export default Navbar;
